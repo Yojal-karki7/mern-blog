059b56f462845fde20e13b07ca85a89d5634a6c8
Yojal-karki7 <yojalkarki@gmail.com>
2024-12-24 14:27:02 +0545

    add show more functionality to the dashboard

--- client/src/components/DashPost.jsx
+++ client/src/components/DashPost.jsx
@@ -6,6 +6,7 @@ import {Link} from 'react-router-dom'
 const DashPost = () => {
   const {currentUser} = useSelector((state) => state.user);
   const [userPosts, setUserPosts] = useState([])
+  const [showMore, setShowMore ] = useState(true)
   
   useEffect(()=>{
     const fetchPosts = async() =>{
@@ -14,6 +15,9 @@ const DashPost = () => {
         const data =  await res.json();
         if(res.ok){
           setUserPosts(data.posts)
+          if(data.posts.length < 9) {
+            setShowMore(false)
+          }
         }
       } catch (error) {
         console.log(error.message);
@@ -23,7 +27,24 @@ const DashPost = () => {
     if(currentUser.isAdmin) {
       fetchPosts();
     }
-  }, [currentUser._id])
+  }, [currentUser._id]);
+
+  const handleShowMore = async()=>{
+    const startIndex = userPosts.length;
+    try {
+      const res = await fetch(`/api/post/getposts?userId=${currentUser._id}&startIndex=${startIndex}`);
+      const data = await res.json();
+      if(res.ok){
+        setUserPosts((prev) => [...prev, ...data.posts]);
+        if(data.posts.length < 9) {
+          setShowMore(false)
+        }
+      }
+    } catch (error) {
+      console.log(error);
+      
+    }
+  }
   return (
     <div className='table-auto overflow-x-scroll md:mx-auto p-3 scrollbar scrollbar-track-slate-100 scrollbar-thumb-slate-300 dark:scrollbar-track-slate-700 dark:scrollbar-thumb-slate-500'>
       {currentUser.isAdmin && userPosts.length > 0 ? (
@@ -68,6 +89,11 @@ const DashPost = () => {
             </Table.Body>
           ))}
         </Table>
+        {
+          showMore && (
+            <button onClick={handleShowMore} className='w-full text-teal-500 text-sm py-7 self-center'>Show more</button>
+          )
+        }
         </>
       ) : (<p>You have no posts to display</p>)} 
     </div>
