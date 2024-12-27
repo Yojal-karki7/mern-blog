1aea20614125752d0da4a363e2476a8def534d62
Yojal-karki7 <yojalkarki@gmail.com>
2024-12-27 10:25:13 +0545

    show the comments of a post

--- api/routes/commentRoutes.js
+++ api/routes/commentRoutes.js
@@ -1,9 +1,10 @@
 import express from "express";
 import { verifyToken } from "../utils/verifyUser.js";
-import { createComment } from "../controllers/commentController.js";
+import { createComment, getPostComment } from "../controllers/commentController.js";
 
 const router = express.Router();
 
 router.post('/create',verifyToken, createComment)
+router.get('/getPostComments/:postId', getPostComment)
 
 export default router;
