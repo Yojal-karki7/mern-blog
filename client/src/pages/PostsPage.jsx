36b09a0f3dcc25b6cc04e657344b0e0288776ce1
Yojal-karki7 <yojalkarki@gmail.com>
2024-12-27 15:35:35 +0545

    add recent article section to the post page

--- client/src/pages/PostsPage.jsx
+++ client/src/pages/PostsPage.jsx
@@ -2,12 +2,16 @@ import React, { useEffect, useState } from 'react'
 import { Link, useParams } from 'react-router-dom'
 import {Button, Spinner} from 'flowbite-react'
 import CommentSection from '../components/CommentSection';
+import PostCard from '../components/PostCard';
 
 const PostsPage = () => {
     const {postSlug} = useParams();
     const [loading, setLoading] = useState(true)
     const [error, setError] = useState(false)
     const [post, setPost] = useState(null)
+    const [recentPosts, setRecentPosts] = useState(null)
+    console.log(recentPosts);
+    
     
     useEffect(()=>{
         const fetchPost = async() => {
@@ -33,6 +37,22 @@ const PostsPage = () => {
         fetchPost()
     },[postSlug])
 
+    useEffect(()=>{
+        try {
+            const fetchRecentPost = async() =>{
+                const res = await fetch('/api/post/getposts?limit=3');
+                const data = await res.json();
+                if(res.ok) {
+                    setRecentPosts(data.posts)
+                }
+            }
+            fetchRecentPost()
+        } catch (error) {
+            console.log(error.message);
+        }
+        
+    }, [])
+
     if (loading) return (
         <div className='flex justify-center items-center min-h-screen'>
             <Spinner size='xl'/>
@@ -52,6 +72,14 @@ const PostsPage = () => {
     <div className="p-3 max-w-2xl mx-auto w-full post-content" dangerouslySetInnerHTML={{__html: post && post.content}} style={{ whiteSpace: "pre-wrap" }}> 
     </div>
     <CommentSection postId={post._id}/>
+
+    <div className="flex flex-col justify-center items-center mb-5">
+        <h1 className='text-xl mt-5'>Recent articles</h1>
+        <div className="flex flex-wrap gap-5 mt-5 justify-center">
+            {recentPosts && 
+            recentPosts.map((post) => <PostCard key={post._id} post={post}/>)}
+        </div>
+    </div>
   </main>
   
 }
