0c63926136e7a75b0d36cee8f65511b7e0a64d65
Yojal-karki7 <yojalkarki@gmail.com>
2024-12-28 13:39:28 +0545

    add pre installation

--- api/index.js
+++ api/index.js
@@ -7,6 +7,7 @@ import postRoutes from './routes/postsRoutes.js'
 import commentRoutes from './routes/commentRoutes.js'
 import cors from 'cors'
 import cookieParser from 'cookie-parser';
+import path from 'path'
 
 dotenv.config();
 
@@ -16,6 +17,7 @@ mongoose.connect(process.env.MONGO).then(()=>{
 }).catch((err)=>{
     console.log(err);
 })
+const __dirname = path.resolve();
 
 const app = express();
 app.use(express.json())
@@ -36,6 +38,12 @@ app.use('/api/user', userRoutes)
 app.use('/api/auth', authRoutes)
 app.use('/api/post', postRoutes)
 app.use('/api/comment', commentRoutes)
+
+app.use(express.static(path.join(__dirname, '/client/dist')))
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'))
+})
 
 
 app.use((err, req, res, next)=>{
