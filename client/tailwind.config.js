4e16ba2bca3ee6387669721d19e8bf22c69eb858
Yojal-karki7 <yojalkarki@gmail.com>
2024-12-28 14:46:03 +0545

    fix module

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,14 +1,17 @@
-const flowbite = require("flowbite-react/tailwind");
+import flowbite from 'flowbite/plugin'; // Correct import for flowbite plugin
 
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-    flowbite.content(),
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}', // Ensure correct paths for your project
+    './node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}', // Add flowbite-react components
   ],
   theme: {
     extend: {},
   },
-  plugins: [flowbite.plugin(),require('tailwind-scrollbar'),],
-}
+  plugins: [
+    flowbite, // Flowbite plugin
+    require('tailwind-scrollbar'), // Scrollbar plugin
+  ],
+};
